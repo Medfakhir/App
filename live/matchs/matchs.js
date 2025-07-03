4f24c9d8eadce1b0aa2cc0ea7acb2bb6fcb0345a
Mohamed Fakhir Fakhir <61033546+Medfakhir@users.noreply.github.com>
2025-07-03 16:57:07 +0100

    Create matchs.js

---
+++ live/matchs/matchs.js
@@ -0,0 +1,12 @@
+{
+  "matches": [
+    {
+      "team1": "USA",
+      "team1_logo": "https://lsm-static-prod.livescore.com/high/enet/6713.png",
+      "team2": "Guatemala",
+      "team2_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Flag_of_Guatemala.svg/330px-Flag_of_Guatemala.svg.png",
+      "m3u_url": "https://rzdtwfb81w5jihkz.buyapartments.online/v3/director/VE1ZmI4NzhmOGI3ZGUwLTU4Y2ItNjJhNC1iYjlmLTYyNGM4ZGZm/master.m3u8?auth_key=1751497556-2151d3bcda5a9d8c8aab41367010aaf7-00000000000000000000000000000000-1abc64e1e62b6e987447d1c8e187ef51"
+    },
+  
+  ],
+}
